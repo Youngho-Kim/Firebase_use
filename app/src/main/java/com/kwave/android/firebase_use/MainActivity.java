481f1b036abaa6d7053641d10c9ad820eecaea82
KWAVE\kwave <kwave55@gmail.com>
2017-06-30 21:09:07 +0900

    익명채팅

--- app/src/main/java/com/kwave/android/firebase_use/MainActivity.java
+++ app/src/main/java/com/kwave/android/firebase_use/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editText;
     Button btnSend;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
